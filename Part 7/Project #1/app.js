1c4e6a23ce0da0c13cb0fae502282a8a4e5d1c6a
Weston Partin <djwest91@gmail.com>
2020-11-18 10:25:03 -0600

    functioning version of application

--- Part 7/Project #1/app.js
+++ Part 7/Project #1/app.js
@@ -38,10 +38,10 @@ const dataStorage = (function () {
   let data;
 
   data = {
-    correctNumber: Math.floor(Math.random() * 20) + 1,
-    score: document.querySelector(".score").textContent,
+    correctNumber: null,
+    score: null,
     highscore: document.querySelector(".highscore").textContent,
-    guess: 0,
+    guess: null,
   };
 
   return {
@@ -58,20 +58,86 @@ const userInterface = (function () {
     clearInput: function () {
       document.querySelector(".guess").value = "";
     },
+    updateMessage: function (status) {
+      document.querySelector(".message").textContent = status;
+    },
+    updateScore: function (number) {
+      document.querySelector(".score").textContent = number;
+    },
+    updateHighscore: function (number) {
+      document.querySelector(".highscore").textContent = number;
+    },
   };
 })();
 
 /// COMPUTE MODULE \\\
 const computeGame = (function (dataS, userI) {
-  // This is the code for receiving our user input & adding it to our data module
-  document.querySelector(".check").addEventListener("click", function () {
+  let guess, correct, score, highscore;
+  // Here is our initialization function
+  const init = function () {
+    console.log("App has started.");
+    dataS.data.score = 20;
+    userI.updateMessage("Start guessing...");
+    userI.updateNumber("?");
+    userI.updateScore(dataS.data.score);
+    dataS.data.correctNumber = Math.floor(Math.random() * 20) + 1;
+    dataS.data.guess = -1;
+  };
+  // This is the code for our CHECK! button
+  const click = function () {
+    // Let's get the guess number from the input field
     dataS.data.guess = parseFloat(document.querySelector(".guess").value);
-
+    // These are our placeholder values so we don't have to type so much
+    guess = dataS.data.guess;
+    correct = dataS.data.correctNumber;
+    score = dataS.data.score;
+    highscore = dataS.data.highscore;
     // Let's run the ui code and show it on the screen
     userI.updateNumber(dataS.data.guess);
-
     // Let's clear the input field after each click
     userI.clearInput();
-    console.log(dataS.data.guess);
+    // Here we will build logic for the game itself
+    if (!isNaN(guess)) {
+      if (guess < 1) {
+        window.alert("Please input a valid number 1-20");
+        userI.updateNumber("?");
+      } else if (guess === correct) {
+        // Here we will use a ternary to either update highscore or not
+        score > highscore ? userI.updateHighscore(score) : exit;
+        userI.updateMessage("🎉 Correct!");
+      } else if (guess < correct) {
+        userI.updateMessage("📉 Too low!");
+        userI.updateScore((score -= 1));
+      } else if (guess > correct) {
+        userI.updateMessage("📈 Too high!");
+        userI.updateScore((score -= 1));
+      } else if (score === 0) {
+        userI.updateMessage("Out of score points!");
+      } else {
+        console.log("A wierd bug has happened");
+      }
+    } else {
+      window.alert("Please input a valid number 1-20");
+      userI.updateNumber("?");
+    }
+    dataS.data.score = score;
+    dataS.data.highscore = highscore;
+    console.log(guess, dataS.data.correctNumber);
+  };
+  // Here are our event listeners
+  document.querySelector(".check").addEventListener("click", click);
+  document.addEventListener("keypress", function () {
+    // Only when user uses the return key
+    if (event.keyCode === 13 || event.which === 13) {
+      // execute our click function
+      click();
+    }
   });
+  // This is the code for our AGAIN! button
+  document.querySelector(".again").addEventListener("click", init);
+
+  return {
+    init,
+  };
 })(dataStorage, userInterface);
+computeGame.init();
