bab84c0f45b97ffbaf3fc352a90849d5fadfe118
Weston Partin <djwest91@gmail.com>
2020-11-27 09:22:10 -0600

    cleaned up document.querySelector not following DRY, updated console.logs

--- Part 7/Project #3 - Pig Game/app.js
+++ Part 7/Project #3 - Pig Game/app.js
@@ -19,160 +19,144 @@ const gameData = (function () {
 /// UI Module \\\
 const userInterface = (function (gameD) {
   const DOMstrings = {
-    player0: ".player--0",
-    player1: ".player--1",
+    player0: document.querySelector(".player--0"),
+    player1: document.querySelector(".player--1"),
     activePlayer: "player--active",
-    score0: "#score--0",
-    score1: "#score--1",
-    name0: "#name--0",
-    name1: "#name--1",
-    current: "#current--",
-    dice: ".dice",
-    new: ".btn--new",
-    roll: ".btn--roll",
-    hold: ".btn--hold",
+    score0: document.querySelector("#score--0"),
+    score1: document.querySelector("#score--1"),
+    name0: document.querySelector("#name--0"),
+    name1: document.querySelector("#name--1"),
+    current0: document.querySelector("#current--0"),
+    current1: document.querySelector("#current--1"),
+    dice: document.querySelector(".dice"),
+    new: document.querySelector(".btn--new"),
+    roll: document.querySelector(".btn--roll"),
+    hold: document.querySelector(".btn--hold"),
   };
   return {
     DOMstrings,
     switchActivePlayer: function (num) {
       // Num will check which activePlayer we are
       if (num === 0) {
-        document
-          .querySelector(DOMstrings.player0)
-          .classList.remove(DOMstrings.activePlayer);
-        document
-          .querySelector(DOMstrings.player1)
-          .classList.add(DOMstrings.activePlayer);
+        DOMstrings.player0.classList.remove(DOMstrings.activePlayer);
+        DOMstrings.player1.classList.add(DOMstrings.activePlayer);
         gameD.data.activePlayer = 1;
       } else {
-        document
-          .querySelector(DOMstrings.player1)
-          .classList.remove(DOMstrings.activePlayer);
-        document
-          .querySelector(DOMstrings.player0)
-          .classList.add(DOMstrings.activePlayer);
+        DOMstrings.player1.classList.remove(DOMstrings.activePlayer);
+        DOMstrings.player0.classList.add(DOMstrings.activePlayer);
         gameD.data.activePlayer = 0;
       }
     },
     clearCurrent: function (activePlayer) {
       activePlayer === 0
-        ? (document.querySelector(DOMstrings.current + "0").textContent = 0)
-        : (document.querySelector(DOMstrings.current + "1").textContent = 0);
+        ? (DOMstrings.current0.textContent = 0)
+        : (DOMstrings.current1.textContent = 0);
     },
     updateScore: function (activePlayer) {
       activePlayer === 0
-        ? (document.querySelector(DOMstrings.score0).textContent =
-            gameD.data.score0)
-        : (document.querySelector(DOMstrings.score1).textContent =
-            gameD.data.score1);
+        ? (DOMstrings.score0.textContent = gameD.data.score0)
+        : (DOMstrings.score1.textContent = gameD.data.score1);
     },
     showDice: function () {
-      document.querySelector(DOMstrings.dice).classList.remove("hidden");
+      DOMstrings.dice.classList.remove("hidden");
     },
     hideDice: function () {
-      document.querySelector(DOMstrings.dice).classList.add("hidden");
+      DOMstrings.dice.classList.add("hidden");
     },
     changeDice: function (num) {
-      document.querySelector(DOMstrings.dice).src = "dice-" + num + ".png";
+      DOMstrings.dice.src = "dice-" + num + ".png";
     },
     addWinner: function (num) {
       num === 0
-        ? (document.querySelector(DOMstrings.name0).textContent =
-            "🏆 WINNER 🏆")
-        : (document.querySelector(DOMstrings.name1).textContent =
-            "🏆 WINNER 🏆");
+        ? (DOMstrings.name0.textContent = "🏆 WINNER 🏆")
+        : (DOMstrings.name1.textContent = "🏆 WINNER 🏆");
       gameD.data.gamePlaying = false;
+      console.log("Application has stopped");
     },
     removeWinner: function () {
       gameD.data.activePlayer === 0
-        ? (document.querySelector(DOMstrings.name0).textContent = "Player 1")
-        : (document.querySelector(DOMstrings.name1).textContent = "Player 2");
+        ? (DOMstrings.name0.textContent = "Player 1")
+        : (DOMstrings.name1.textContent = "Player 2");
     },
   };
 })(gameData);
 
 /// Logic Module \\\
 const gameLogic = (function (gameD, userI) {
-  let dice, score;
+  let dice;
   // Here is our roll button functionality
-  document
-    .querySelector(userI.DOMstrings.roll)
-    .addEventListener("click", function () {
-      // Let's get our dice roll
-      dice = Math.floor(Math.random() * 6) + 1;
-      // Is our game still playing?
-      if (gameD.data.gamePlaying) {
-        // Is our dice NOT 1
-        if (dice !== 1) {
-          userI.showDice();
-          userI.changeDice(dice);
-          gameD.data.current += dice;
-          document.querySelector(
-            // We are using the hard-coded DOM bit from userI, with the dynamic number of activePlayer
-            userI.DOMstrings.current + gameD.data.activePlayer
-          ).textContent = gameD.data.current;
-          // What if our dice IS 1?
-        } else {
-          gameD.data.current = 0;
-          userI.hideDice();
-          userI.clearCurrent(gameD.data.activePlayer);
-          userI.switchActivePlayer(gameD.data.activePlayer);
-        }
-      }
-    });
-  // Here is our Hold button functionality
-  document
-    .querySelector(userI.DOMstrings.hold)
-    .addEventListener("click", function () {
-      // Is our game playing?
-      if (gameD.data.gamePlaying) {
-        // Are we player 1 or player 2?
-        if (gameD.data.activePlayer === 0) {
-          gameD.data.score0 += gameD.data.current;
-          gameD.data.current = 0;
-          userI.updateScore(gameD.data.activePlayer);
-          // Did we win?
-          if (gameD.data.score0 >= gameD.data.winningScore)
-            userI.addWinner(gameD.data.activePlayer);
-        } else {
-          gameD.data.score1 += gameD.data.current;
-          gameD.data.current = 0;
-          userI.updateScore(gameD.data.activePlayer);
-          // Did we win?
-          if (gameD.data.score1 >= gameD.data.winningScore)
-            userI.addWinner(gameD.data.activePlayer);
-        }
-      }
-      // If there isn't a winner yet let's swap players
-      if (gameD.data.gamePlaying) {
+  userI.DOMstrings.roll.addEventListener("click", function () {
+    // Let's get our dice roll
+    dice = Math.floor(Math.random() * 6) + 1;
+    // Is our game still playing?
+    if (gameD.data.gamePlaying) {
+      // Is our dice NOT 1
+      if (dice !== 1) {
+        userI.showDice();
+        userI.changeDice(dice);
+        gameD.data.current += dice;
+        // We are using the hard-coded DOM bit from userI, with the dynamic number of activePlayer
+        gameD.data.activePlayer === 0
+          ? (userI.DOMstrings.current0.textContent = gameD.data.current)
+          : (userI.DOMstrings.current1.textContent = gameD.data.current);
+        // What if our dice IS 1?
+      } else {
+        gameD.data.current = 0;
         userI.hideDice();
         userI.clearCurrent(gameD.data.activePlayer);
         userI.switchActivePlayer(gameD.data.activePlayer);
-        // If someone won, let's stop roll and hold from working
-      } else {
-        userI.hideDice();
-        userI.clearCurrent(gameD.data.activePlayer);
       }
-    });
+    }
+  });
+  // Here is our Hold button functionality
+  userI.DOMstrings.hold.addEventListener("click", function () {
+    // Is our game playing?
+    if (gameD.data.gamePlaying) {
+      // Are we player 1 or player 2?
+      if (gameD.data.activePlayer === 0) {
+        gameD.data.score0 += gameD.data.current;
+        gameD.data.current = 0;
+        userI.updateScore(gameD.data.activePlayer);
+        // Did we win?
+        if (gameD.data.score0 >= gameD.data.winningScore)
+          userI.addWinner(gameD.data.activePlayer);
+      } else {
+        gameD.data.score1 += gameD.data.current;
+        gameD.data.current = 0;
+        userI.updateScore(gameD.data.activePlayer);
+        // Did we win?
+        if (gameD.data.score1 >= gameD.data.winningScore)
+          userI.addWinner(gameD.data.activePlayer);
+      }
+    }
+    // If there isn't a winner yet let's swap players
+    if (gameD.data.gamePlaying) {
+      userI.hideDice();
+      userI.clearCurrent(gameD.data.activePlayer);
+      userI.switchActivePlayer(gameD.data.activePlayer);
+      // If someone won, let's stop roll and hold from working
+    } else {
+      userI.hideDice();
+      userI.clearCurrent(gameD.data.activePlayer);
+    }
+  });
   // This is our New Game button functionality
-  document
-    .querySelector(userI.DOMstrings.new)
-    .addEventListener("click", function () {
-      userI.removeWinner();
-      gameLogic.init();
-      userI.switchActivePlayer();
-    });
+  userI.DOMstrings.new.addEventListener("click", function () {
+    userI.removeWinner();
+    gameLogic.init();
+    userI.switchActivePlayer();
+  });
 
   return {
     // This function will 'initialize' the game on first load, and each time New Game is clicked
     init: function () {
-      console.log("Application is running");
+      console.log("Application has started.");
       userI.removeWinner();
-      document.querySelector(userI.DOMstrings.score0).textContent = 0;
-      document.querySelector(userI.DOMstrings.score1).textContent = 0;
       userI.hideDice();
-      document.querySelector(userI.DOMstrings.current + "0").textContent = 0;
-      document.querySelector(userI.DOMstrings.current + "1").textContent = 0;
+      userI.DOMstrings.score0.textContent = 0;
+      userI.DOMstrings.score1.textContent = 0;
+      userI.DOMstrings.current0.textContent = 0;
+      userI.DOMstrings.current1.textContent = 0;
       gameD.data.gamePlaying = true;
       gameD.data.activePlayer = 0;
       gameD.data.current = 0;
