8d4c66b57f1bcf713da040f1555fadcd87223d5d
Weston Partin <djwest91@gmail.com>
2020-12-08 14:12:09 -0600

    completed challenge files

---
+++ Part 9/Coding Challenge #1/app.js
@@ -0,0 +1,67 @@
+"use strict";
+
+const game = {
+  team1: "Bayern Munich",
+  team2: "Borrussia Dortmund",
+  players: [
+    [
+      "Neuer",
+      "Pavard",
+      "Martinez",
+      "Alaba",
+      "Davies",
+      "Kimmich",
+      "Goretzka",
+      "Coman",
+      "Muller",
+      "Gnarby",
+      "Lewandowski",
+    ],
+    [
+      "Burki",
+      "Schulz",
+      "Hummels",
+      "Akanji",
+      "Hakimi",
+      "Weigl",
+      "Witsel",
+      "Hazard",
+      "Brandt",
+      "Sancho",
+      "Gotze",
+    ],
+  ],
+  score: "4:0",
+  scored: ["Lewandowski", "Gnarby", "Lewandowski", "Hummels"],
+  date: "Nov 9th, 2037",
+  odds: {
+    team1: 1.33,
+    x: 3.25,
+    team2: 6.5,
+  },
+};
+
+// 1)
+const [players1, players2] = game.players;
+// 2)
+const [gk, ...fieldPlayers] = players1;
+// 3)
+const allPlayers = [...players1, ...players2];
+// 4)
+const players1Final = [...players1, "Thiago", "Coutinho", "Perisic"];
+// 5)
+const {
+  odds: { team1, x: draw, team2 },
+} = game;
+console.log(team1, draw, team2);
+// 6)
+const printGoals = function (...playerNames) {
+  for (let i = 0; i < playerNames.length; i++) {
+    console.log(`${playerNames[i]} scored a goal!`);
+  }
+  console.log(`The total score was ${playerNames.length}`);
+};
+// 7)
+team1 < team2 && console.log("Team 1 will win!");
+team1 > team2 && console.log("Team 2 will win!");
+printGoals("Davies", "Muller", "Lewandowski", "Kimmich");
