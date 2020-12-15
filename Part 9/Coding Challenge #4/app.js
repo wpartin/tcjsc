48d6f23fc9fc7c0c8fe20683b15654807e5819f8
Weston Partin <djwest91@gmail.com>
2020-12-15 12:22:29 -0600

    completed challenge

---
+++ Part 9/Coding Challenge #4/app.js
@@ -0,0 +1,18 @@
+"use stict";
+
+document.body.append(document.createElement("textarea"));
+document.body.append(document.createElement("button"));
+
+document.querySelector("button").addEventListener("click", function () {
+  const text = document.querySelector("textarea").value;
+  const noNewLineList = text.split("\n");
+
+  for (const [i, row] of noNewLineList.entries()) {
+    const [first, second] = row.toLowerCase().trim().split("_");
+    const output = `${first}${second.replace(
+      second[0],
+      second[0].toUpperCase()
+    )}`;
+    console.log(`${output.padEnd(20)}${"✅".repeat(i + 1)}`);
+  }
+});
