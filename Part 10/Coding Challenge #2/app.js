ee2fec889dd43b9bf42248c239e59ba32c9ea88c
Weston Partin <djwest91@gmail.com>
2020-12-18 09:29:14 -0600

    completed challenge

--- Part 10/Coding Challenge #2/app.js
+++ Part 10/Coding Challenge #2/app.js
@@ -1,1 +1,11 @@
 "use strict";
+
+(function () {
+  const header = document.querySelector("h1");
+  header.style.color = "red";
+
+  const body = document.querySelector("body");
+  body.addEventListener("click", function () {
+    header.style.color = "blue";
+  });
+})();
