5cabcae3485cf0b04a1adf92eb675f93c84d3d30
Weston Partin <djwest91@gmail.com>
2020-12-22 19:53:33 -0600

    partial update to app.js

--- Part 11/Coding Challenge #1/app.js
+++ Part 11/Coding Challenge #1/app.js
@@ -75,7 +75,41 @@ const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 /////////////////////////////////////////////////
 
+/// Slice - Doesn't change the original array!
+console.log(`Here is slice`);
+console.log(`OG Arr ------/`);
 let arr = ["a", "b", "c", "d", "e"];
-
+console.log(arr);
 console.log(arr.slice(2));
 console.log(arr.slice(2, 4));
+console.log(arr.slice(-2));
+console.log(arr.slice(-1));
+console.log(arr.slice(1, -2));
+console.log(arr.slice());
+
+/// Splice - Does change the original array!
+
+console.log(`Here is splice`);
+// console.log(arr.splice(2));
+console.log(arr);
+console.log(`OG Arr ------/`);
+arr.splice(-1);
+arr.splice(1, 2);
+console.log(arr);
+
+/// Reverse
+
+arr = ["a", "b", "c", "d", "e"];
+const arr2 = ["j", "i", "h", "g", "f"];
+console.log(arr2.reverse());
+
+/// Concat
+
+const letters = arr.concat(arr2);
+console.log(letters);
+/// Same as above, but doesn't mutate any array
+// console.log([...arr, ...arr2]);
+
+/// Join
+
+console.log(letters.join(" - "));
